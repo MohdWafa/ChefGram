19bd8461a9d74fb58c8dcc877fcd2a57c078898c
MohdWafa <62431429+MohdWafa@users.noreply.github.com>
2022-01-08 20:15:31 +0100

    created working links in nav and landing pagers

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar.js';
+import Landing from './components/layout/Landing.js';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </section>
+      {/* <Routes>
+       
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
+      </Routes> */}
+    </Fragment>
+  </Router>
 );
 
 export default App;
