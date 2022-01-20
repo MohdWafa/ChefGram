3c2ace8ac5b702b0537e403a61ecd34dbe3e134e
MohdWafa <62431429+MohdWafa@users.noreply.github.com>
2022-01-20 14:14:09 +0100

    made store for redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,35 @@ import Navbar from './components/layout/Navbar.js';
 import Landing from './components/layout/Landing.js';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      {/* <Routes>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        {/* <Routes>
         <Route path='/' element={<Landing />} />
       </Routes> */}
 
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='register' element={<Register />} />
-        <Route path='login' element={<Login />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
 
-      {/* <Routes>
+        {/* <Routes>
        
         <Route path='register' element={<Register />} />
         <Route path='login' element={<Login />} />
       </Routes> */}
-    </Fragment>
-  </Router>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
