09afaf341e3e0b1c1ca5fb5de68a620dcf578b04
MohdWafa <62431429+MohdWafa@users.noreply.github.com>
2022-02-20 18:00:33 +0100

    dashboard update

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 //Load User
@@ -90,5 +91,6 @@ export const login = (email, password) => async (dispatch) => {
 //Logout / Clear Profile
 
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
