30b2e4369626e93839d0ee022f289398106ef214
MohdWafa <62431429+MohdWafa@users.noreply.github.com>
2022-02-20 01:25:46 +0100

    protectedroute for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Landing from './components/layout/Landing.js';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -35,6 +38,10 @@ const App = () => {
               {/* <Route path='/' element={<Landing />} /> */}
               <Route path='register' element={<Register />} />
               <Route path='login' element={<Login />} />
+              <Route
+                path='dashboard'
+                element={<PrivateRoute component={Dashboard} />}
+              />
             </Routes>
           </section>
 
